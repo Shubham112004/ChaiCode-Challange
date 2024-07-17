c0b1c8fbea6ff9c7c5adb139d4f9f6f38d54a018
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-07-17 16:10:02 +0530

    day4 completed

---
+++ Day-4/task3.js
@@ -0,0 +1,8 @@
+let num = 1
+let newNum = 0
+while (num <= 10) {
+    newNum = num + newNum
+    num++
+}
+
+console.log(newNum);
---
+++ Day-4/task6.js
@@ -0,0 +1,8 @@
+let number = 4;
+let result = 1;
+do {
+    result *= number;
+    number--;
+} while (number > 0);
+
+console.log(result);
